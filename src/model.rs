565b59d0c8137a114ba71fc2a04e28895958924e
Antonio Mauricio <mauricio.almeida.dev@gmail.com>
2025-09-01 17:06:06 -0300

    feat: adds update_json_database func

--- src/model.rs
+++ src/model.rs
@@ -9,3 +9,9 @@ pub fn read_json_database_file() -> StudyCycle {
         serde_json::from_str(&content).expect("failed to parse database content");
     study_cycle
 }
+
+pub fn udpate_json_database(data: StudyCycle) {
+    let json_data = serde_json::to_string_pretty::<StudyCycle>(&data)
+        .expect("failed to parse new data into json string");
+    fs::write("./database.json", json_data).expect("failed to write into database file");
+}
