c539fdb4a0fe979169ad0c24039beb2035f25978
Antonio Mauricio <mauricio.almeida.dev@gmail.com>
2025-09-03 17:23:21 -0300

    adds a temporary fix to the problem by sorting arrays and using them

--- src/utils.rs
+++ src/utils.rs
@@ -47,6 +47,7 @@ pub fn display_table(subjects: Vec<Subject>) {
 
 pub fn display_table_with_progress_bar(subjects: Vec<Subject>) {
     let subject_names: Vec<String> = subjects.iter().map(|s| s.name.clone()).collect();
+    let mut temp_subjects = subjects.clone();
     let headers: Vec<String> = vec![
         "NAME".to_owned(),
         "PROGRESS BAR".to_owned(),
@@ -54,38 +55,35 @@ pub fn display_table_with_progress_bar(subjects: Vec<Subject>) {
     ];
     let biggest_cell = get_biggest_string_len(subject_names);
     let biggest_header = get_biggest_string_len(headers);
-    if biggest_cell > biggest_header {
+    temp_subjects.sort_by_key(|s| s.max_study_hours);
+    let biggest_progress_bar = temp_subjects
+        .last()
+        .expect("failed to get last subject")
+        .clone();
+    let mut possible_values_to_space_columns = vec![
+        biggest_cell,
+        biggest_header,
+        biggest_progress_bar.max_study_hours as usize,
+    ];
+    possible_values_to_space_columns.sort();
+    let space_between_columns = possible_values_to_space_columns
+        .last()
+        .expect("failed to get the last possible vlaes to space columns")
+        .clone();
+    println!(
+        "{0: <space_between_columns$} | {1: <space_between_columns$} | {2: <space_between_columns$}",
+        "NAME", "PROGRESS BAR", "HOURS"
+    );
+    for subject in subjects {
         println!(
-            "{0: <biggest_cell$} | {1: <biggest_cell$} | {2: <biggest_cell$}",
-            "NAME", "PROGRESS BAR", "HOURS"
+            "{0: <space_between_columns$} | {1: <space_between_columns$} | {2: <space_between_columns$}",
+            subject.name,
+            get_study_hours_progress_bar(
+                subject.studied_hours.into(),
+                subject.max_study_hours.into()
+            ),
+            format!("{}/{}h", subject.studied_hours, subject.max_study_hours)
         );
-        for subject in subjects {
-            println!(
-                "{0: <biggest_cell$} | {1: <biggest_cell$} | {2: <biggest_cell$}",
-                subject.name,
-                get_study_hours_progress_bar(
-                    subject.studied_hours.into(),
-                    subject.max_study_hours.into()
-                ),
-                format!("{}/{}h", subject.studied_hours, subject.max_study_hours)
-            );
-        }
-    } else {
-        println!(
-            "{0: <biggest_header$} | {1: <biggest_header$} | {2: <biggest_header$}",
-            "NAME", "PROGRESS BAR", "HOURS"
-        );
-        for subject in subjects {
-            println!(
-                "{0: <biggest_header$} | {1: <biggest_header$} | {2: <biggest_header$}",
-                subject.name,
-                get_study_hours_progress_bar(
-                    subject.studied_hours.into(),
-                    subject.max_study_hours.into()
-                ),
-                format!("{}/{}h", subject.studied_hours, subject.max_study_hours)
-            );
-        }
     }
 }
 
