e65c9559b5f9f137fae0515a2814f6a8d65aae9f
Antonio Mauricio <mauricio.almeida.dev@gmail.com>
2025-09-03 19:06:38 -0300

    adds alias, names and removes short and long from args so they can be used as value and not flag

--- src/commands.rs
+++ src/commands.rs
@@ -1,27 +1,34 @@
 use clap::{Args, Subcommand};
 
-#[derive(Subcommand)]
+#[derive(Subcommand, Debug)]
 pub enum Commands {
+    #[command(alias = "s")]
     Study(SutdyArgs),
+
+    #[command(alias = "v")]
     View(ViewArgs),
+
+    #[command(alias = "r")]
     Reset,
+
+    #[command(name = "seed")]
     SeedDatabase(SeedDatabaseArgs),
 }
 
-#[derive(Args)]
+#[derive(Args, Debug)]
 pub struct ViewArgs {
     #[arg(short, long)]
     pub all: bool,
 }
 
-#[derive(Args)]
+#[derive(Args, Debug)]
 pub struct SutdyArgs {
-    #[arg(short, long)]
+    #[arg()]
     pub name: String,
 }
 
-#[derive(Args)]
+#[derive(Args, Debug)]
 pub struct SeedDatabaseArgs {
-    #[arg(short, long)]
+    #[arg()]
     pub path: String,
 }
