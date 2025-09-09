b4eae854c68e220347908d09165f90dbebdad14b
Antonio Mauricio <mauricio.almeida.dev@gmail.com>
2025-09-09 14:56:26 -0300

    uses new handler and storage in main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 
 use crate::commands::Commands;
+use crate::error::AppError;
 
 mod commands;
 mod error;
@@ -17,13 +18,18 @@ struct Cli {
     command: Commands,
 }
 
-fn main() {
+fn main() -> Result<(), AppError> {
     let cli = Cli::parse();
 
+    let storage: Box<dyn storage::Storage> =
+        Box::new(storage::json_storage::JsonStorage::new("./database.json"));
+    let handler: Box<dyn handlers::Handler> = Box::new(handlers::v1::V1Handler::new(storage));
+
     match &cli.command {
-        Commands::Study(args) => handlers::study_subject(args.name.clone()),
-        Commands::View(args) => handlers::view_study_cycle(args.all),
-        Commands::Reset => handlers::reset_cycle(),
-        Commands::SeedDatabase(args) => handlers::seed_database(args.path.clone()),
-    }
+        Commands::Study(args) => handler.study_cycle_by_name(&args.name)?,
+        Commands::View(args) => handler.show_cycle(args.all)?,
+        Commands::Reset => handler.reset_cycle()?,
+    };
+
+    Ok(())
 }
