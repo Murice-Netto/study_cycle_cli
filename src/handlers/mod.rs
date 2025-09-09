ea47cd391b2a1a82913ab0b18c2625cd96f8b79d
Antonio Mauricio <mauricio.almeida.dev@gmail.com>
2025-09-09 14:23:46 -0300

    adds handler trait

---
+++ src/handlers/mod.rs
@@ -0,0 +1,9 @@
+mod v1;
+
+use crate::error::AppError;
+
+pub trait Handler {
+    fn study_cycle_by_name(&self, name: &str) -> Result<(), AppError>;
+    fn show_cycle(&self, all: bool) -> Result<(), AppError>;
+    fn reset_cycle(&self) -> Result<(), AppError>;
+}
